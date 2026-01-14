953093b22a542fc624a94a096e74f9b0be1a6127
Sibimadhavan <148133428+Sibimadhavan@users.noreply.github.com>
2026-01-14 12:49:51 +0530

    Update db_mysql.php

--- PHP/db_mysql.php
+++ PHP/db_mysql.php
@@ -1,8 +1,9 @@
 <?php
-$conn = new mysqli("127.0.0.1", "guviuser", "Guvi@1234", "guvi_auth", 3306);
+$conn = new mysqli("127.0.0.1", "root", "1234", "guvi_auth", 3306);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 
 ?>
+
